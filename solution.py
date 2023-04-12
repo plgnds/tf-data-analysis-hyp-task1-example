f2e07f86d9e9d65056089e6e866cda74a003624f
plgnds <127914162+plgnds@users.noreply.github.com>
2023-04-12 11:23:48 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 523034793 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+import scipy.stats as stats
+
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+  p1 = x_success / x_cnt
+  p2 = y_success / y_cnt
+  p = (x_success + y_success) / (x_cnt + y_cnt)
+  se = np.sqrt(p * (1 - p) * (1/x_cnt + 1/y_cnt))
+  z = (p2 - p1) / se
+  if z > stats.norm.ppf(1 - 0.03/2):
+    return True
+  else:
+    return False
