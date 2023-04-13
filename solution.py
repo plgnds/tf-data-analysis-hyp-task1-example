a8110fe9e88943682dc56ed6815ef31070bd6d22
plgnds <127914162+plgnds@users.noreply.github.com>
2023-04-13 18:22:37 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,17 +6,17 @@ from statsmodels.stats.proportion import proportions_ztest
 
 
 chat_id = 523034793 # Ваш chat ID, не меняйте название переменной
-
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-  
-    alpha=0.06
-    
-    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] / 2
-    
-    if (p_value > alpha) and (x_success/x_cnt > y_success/y_cnt):
-      return True
-    else: 
-      return False
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (p1 - p2) / np.sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt))
+    p_value = 2 * (1 - norm.cdf(abs(z)))
+    alpha = 0.03
+    return  p_value > alpha 
