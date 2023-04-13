c474126e6bdcdd8045a890a873d34be79d0ce685
plgnds <127914162+plgnds@users.noreply.github.com>
2023-04-13 18:21:03 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
 
 
 chat_id = 523034793 # Ваш chat ID, не меняйте название переменной
 
-import scipy.stats as stats
-
-def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-  p1 = x_success / x_cnt
-  p2 = y_success / y_cnt
-  p = (x_success + y_success) / (x_cnt + y_cnt)
-  se = np.sqrt(p * (1 - p) * (1/x_cnt + 1/y_cnt))
-  z = (p2 - p1) / se
-  if z > stats.norm.ppf(1 - 0.03/2):
-    return True
-  else:
-    return False
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+  
+    alpha=0.06
+    
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] / 2
+    
+    if (p_value > alpha) and (x_success/x_cnt > y_success/y_cnt):
+      return True
+    else: 
+      return False
